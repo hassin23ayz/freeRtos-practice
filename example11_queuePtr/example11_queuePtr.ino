72d4cfa35379ccdd18e50acf54e2c4d23d3d139c
ayz03 <hassin23ayz@gmail.com>
2019-06-22 12:25:49 +0600

    example11_queuePtr shows how to pass character array in queue

--- example11_queuePtr/example11_queuePtr.ino
+++ example11_queuePtr/example11_queuePtr.ino
@@ -5,17 +5,31 @@ this example shows how to pass data via a pointer of a buffer array using freert
 #include <Arduino_FreeRTOS.h>
 #include <queue.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+
 // declare a queue variable of type QueueHandle_t . 
 QueueHandle_t xPointerQueue;
+
+typedef enum
+{
+  eGPS,
+  eCAN
+}EDataSource;
+
+typedef struct 
+{
+  char* dataPtr;
+  EDataSource eDataSrc;
+}SData;
 
 void setup() {
   Serial.begin(9600);
   while (!Serial) {;}
   delay(1000);
   Serial.println("-------FreeRtos------");
-  // create the Queue to hold a maximum of 5 items of type char* 
-  //xPointerQueue = xQueueCreate(5, sizeof(char*));
-  xPointerQueue = xQueueCreate(1, sizeof(uint32_t));
+
+  xPointerQueue = xQueueCreate(1, sizeof(SData));
 
   if(xPointerQueue != NULL)
   {
@@ -36,22 +50,19 @@ void loop()
 static void vSenderTask(void* pvParameters)
 {
   BaseType_t xStatus;
-  char* dataToSend;
-  //BaseType_t xDataCounter = 0;
-  uint32_t xDataCounter = 0;
+  SData gps1Data;
+
+  gps1Data.eDataSrc = eGPS;
+  gps1Data.dataPtr = "dhaka,bangladesh";
 
   for(;;)
-  {
-    //dataToSend = (char*) malloc(64 * sizeof(char));
-    //snprintf(dataToSend, 64, "Data-ID %d = Hello RTOS/r/n", xDataCounter);
-    //xStatus = xQueueSendToBack(xPointerQueue, &dataToSend, 0);
-    xStatus = xQueueSendToBack(xPointerQueue, &xDataCounter, portMAX_DELAY);
+  {    
+    xStatus = xQueueSendToBack(xPointerQueue, &gps1Data, 0);
+    //xStatus = xQueueSendToBack(xPointerQueue, &xDataCounter, portMAX_DELAY);
 
     if(xStatus == pdPASS)
     {
-      Serial.print("data sent to queue = ");
-      Serial.println(xDataCounter);
-      xDataCounter++;
+      Serial.println("data sent to queue");
     }
   }
 }
@@ -59,10 +70,8 @@ static void vSenderTask(void* pvParameters)
 static void vReceieverTask(void* pvParameters)
 {
   BaseType_t xStatus;
-  //char* dataToRecv;
   //const TickType_t xTicksToWait = pdMS_TO_TICKS(100);
-
-  uint32_t dataToRecv;
+  SData gpsRcvData;
   for(;;)
   {
 
@@ -71,14 +80,12 @@ static void vReceieverTask(void* pvParameters)
     //   Serial.println(" Queue should have been empty \r\n");
     // }
 
-    xStatus = xQueueReceive(xPointerQueue, (void*)&dataToRecv, portMAX_DELAY);
+    xStatus = xQueueReceive(xPointerQueue, &gpsRcvData, portMAX_DELAY);
 
     if(xStatus == pdPASS)
     {
-      Serial.print("receieved= ");
-      Serial.println(dataToRecv);
-
-    //  free(dataToRecv);
+      Serial.println("receieved= ");
+      Serial.println(gpsRcvData.dataPtr);
     }
     else
     {
