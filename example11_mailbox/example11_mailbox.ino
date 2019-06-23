1ab853cae768ead0af05bc96d957b7d06fb5d7da
ayz03 <hassin23ayz@gmail.com>
2019-06-23 13:02:49 +0600

    example11_mailbox example shows that the xQueuePeek always reads data from mailbox

---
+++ example11_mailbox/example11_mailbox.ino
@@ -0,0 +1,96 @@
+/*
+this example shows the use of queue as a mailbox
+create the queue as usual 
+write to the queue using xQueueOverwrite(QueueHandle_t xQueue, const void* pvItemToQueue)
+read from the queue using xQueuePeek(QueueHandle_t xQueue, void* const pvBuffer, TickType_t xTicksToWait)
+*/
+
+#include <Arduino_FreeRTOS.h>
+#include <queue.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* declare a queue variable of type QueueHandle_t */
+QueueHandle_t xMailBox;
+
+typedef enum
+{
+  E_GPS,
+  E_CAN
+}EDataSource;
+
+typedef struct 
+{
+  EDataSource eDataSrc;
+  TickType_t xTimeStamp; 
+  char cDataBuf[32];
+}SData;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {;}
+  delay(1000);
+  Serial.println("-------FreeRtos------");
+
+  xMailBox = xQueueCreate(1, sizeof(SData));
+
+  if(xMailBox != NULL)
+  {
+    xTaskCreate(prvSenderTask,     "Sender",    1000, NULL, 1, NULL);
+    xTaskCreate(prvReceieverTask, "Receiever", 1000, NULL, 1, NULL);
+    vTaskStartScheduler();
+  }
+}
+
+void loop()
+{
+  /* Empty. Things are done in Tasks */
+  /* should never reach here */
+}
+
+/*---------------------- Tasks ---------------------*/
+static void prvSenderTask(void* pvParameters)
+{
+  BaseType_t xStatus;
+  SData xCanData;
+  xCanData.eDataSrc = E_CAN;
+  uint8_t ucSpeed = 0;
+
+  for(;;)
+  { 
+    xCanData.xTimeStamp = xTaskGetTickCount();
+    memset(xCanData.cDataBuf, '\0', 32);
+    snprintf(xCanData.cDataBuf, 32, "speed:%d time:%ld", ucSpeed++, xCanData.xTimeStamp);
+
+    xStatus = xQueueOverwrite(xMailBox, &xCanData);
+
+    if(xStatus == pdPASS)
+    {
+      Serial.print("data sent to mailbox @");
+      Serial.println(xCanData.xTimeStamp);
+      Serial.println("---------------------------------");
+    }
+  }
+}
+
+static void prvReceieverTask(void* pvParameters)
+{
+  BaseType_t xStatus;
+  SData xCanRcvData;
+
+  for(;;)
+  {
+    xStatus = xQueuePeek(xMailBox, &xCanRcvData, portMAX_DELAY);
+
+    if(xStatus == pdPASS)
+    {
+      Serial.print("\n\rToyota Axio= ");
+      Serial.println(xCanRcvData.cDataBuf);
+    }
+    else
+    {
+      Serial.println("could not receive from the mailbox , timeout");
+    }
+  }
+}
